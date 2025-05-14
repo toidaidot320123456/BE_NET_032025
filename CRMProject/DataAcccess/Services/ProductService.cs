9b5bf59b7fbdb7a05a79b4255cac332f8366cf57
Your Name <toidaidot320123456@gmail.com>
2025-05-14 22:07:37 +0700

    add, edit, update

---
+++ CRMProject/DataAcccess/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using DataAcccess.DBContext;
+using DataAcccess.IRepositories;
+using DataAcccess.IServices;
+using DataAcccess.RequestData;
+
+namespace DataAcccess.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public Task<int> Insert(CreateProduct productRequest)
+        {
+            var product = new Product()
+            {
+                ProductName = productRequest.ProductName,
+                ImagePath = productRequest.ImagePath,
+                Price = productRequest.Price
+            };
+            return _productRepository.Insert(product);
+        }
+        public Task<int> Update(EditProduct productRequest)
+        {
+            var product = new Product()
+            {
+                ProductName = productRequest.ProductName,
+                ImagePath = productRequest.ImagePath,
+                Price = productRequest.Price,
+                Id = productRequest.Id
+            };
+            return _productRepository.Update(product);
+        }
+        public Task<int> Remove(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product != null)
+            {               
+                return _productRepository.Remove(product);
+            }
+            return Task.FromResult(0);
+        }
+        public async Task<List<Product>> GetAll()
+        {
+            return await _productRepository.GetAll();
+        }
+    }
+}
